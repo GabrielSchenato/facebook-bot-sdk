8c2704728d2dd5f225072dd1e226866de151cc99
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-06-15 09:06:12 -0300

    templates criados

---
+++ src/TemplatesMessage/GenericTemplate.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace CodeBot\TemplatesMessage;
+
+use CodeBot\Element\ElementInterface;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ButtonsTemplate
+ *
+ * @author gabriel
+ */
+class GenericTemplate implements TemplateInterface
+{
+
+    protected $products = [];
+    protected $recipientId;
+
+    public function __construct(string $recipientId)
+    {
+        $this->recipientId = $recipientId;
+    }
+
+    public function add(ElementInterface $element)
+    {
+        $this->products[] = $element->get();
+    }
+
+    public function message(string $messageText): array
+    {
+        return [
+            'recipient' => [
+                'id' => $this->recipientId
+            ],
+            'message' => [
+                'attachment' => [
+                    'type' => 'template',
+                    'payload' => [
+                        'template_type' => 'generic',
+                        'buttons' => $this->products
+                    ]
+                ]
+            ]
+        ];
+    }
+
+}
---
+++ src/TemplatesMessage/ListTemplate.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace CodeBot\TemplatesMessage;
+
+use CodeBot\Element\ElementInterface;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ButtonsTemplate
+ *
+ * @author gabriel
+ */
+class ListTemplate implements TemplateInterface
+{
+
+    protected $products = [];
+    protected $recipientId;
+
+    public function __construct(string $recipientId)
+    {
+        $this->recipientId = $recipientId;
+    }
+
+    public function add(ElementInterface $element)
+    {
+        $this->products[] = $element->get();
+    }
+
+    public function message(string $messageText): array
+    {
+        return [
+            'recipient' => [
+                'id' => $this->recipientId
+            ],
+            'message' => [
+                'attachment' => [
+                    'type' => 'template',
+                    'payload' => [
+                        'template_type' => 'list',
+                        'text' => $messageText,
+                        'buttons' => $this->products
+                    ]
+                ]
+            ]
+        ];
+    }
+
+}
