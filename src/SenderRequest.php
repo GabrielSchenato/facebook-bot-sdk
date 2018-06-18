0d3a846cee78c7d37518732334c3ad7f9e7097d3
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-06-18 09:54:18 -0300

    adicionado return do payload

--- src/SenderRequest.php
+++ src/SenderRequest.php
@@ -41,7 +41,7 @@ class SenderRequest
             return null;
         }
         if (is_array($this->event['postback']) and !empty($this->event['postback']['payload'])) {
-            $this->event['postback']['payload'];
+            return $this->event['postback']['payload'];
         }
         return $this->event['postback'];
     }
